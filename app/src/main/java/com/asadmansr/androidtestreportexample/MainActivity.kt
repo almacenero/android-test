1da977b9f0b2b52eaec84ce924c5e4ab7bdb5cbb
Yasser <yassergeo@gmail.com>
2021-07-16 11:43:38 -0500

    added appcenter ui tester

--- app/src/main/java/com/asadmansr/androidtestreportexample/MainActivity.kt
+++ app/src/main/java/com/asadmansr/androidtestreportexample/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.asadmansr.androidtestreportexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "028c9ddf-79b0-4e5f-a57b-260b016a7bc0",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
